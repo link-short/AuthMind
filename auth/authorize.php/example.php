272588a04c87de20c64f42fab0fa47595236ffb5
link-short <dora.family228@gmail.com>
2025-11-24 10:52:15 +0500

    Create example.php

---
+++ auth/authorize.php/example.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>exa</title>
+</head>
+<body>
+<?php
+  
+  echo "Получены параметры с сервера: param1 = ".$_GET['param1']." и param2 = ".$_GET['param2'];
+?>
+</body>
+</html>
