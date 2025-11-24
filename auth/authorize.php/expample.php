0d58abab34aaa981aa289b012af3b505d5de4f5a
link-short <dora.family228@gmail.com>
2025-11-24 10:57:46 +0500

    Update expample.php

--- auth/authorize.php/expample.php
+++ auth/authorize.php/expample.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>exa</title>
-</head>
-<body>
 <?php
   
   echo "Получены параметры с сервера: param1 = ".$_GET['param1']." и param2 = ".$_GET['param2'];
 ?>
-</body>
-</html>
